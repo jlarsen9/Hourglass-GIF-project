eacafcc95b1f3455c7488a4a68af2803b64a4282
Jenny Larsen <jennylarsen91@gmail.com>
2019-05-04 08:56:10 -0700

    message

--- script.js
+++ script.js
@@ -2,7 +2,15 @@
 // document.body.appendChild(button);
 // button.innerHTML = "CHANGE IMAGE";
 
+// Global Variable == BAD
+var startButton = document.getElementById("startButton")
+startButton.addEventListener('click', startGif)
+startButton.addEventListener('click', startAlert)
 
+var stopButton = document.getElementById("stopButton")
+stopButton.addEventListener('click', stopGif)
+
+var intervalId;
 function startGif() {
     intervalId = setInterval(changeImage, 50);
 }
@@ -11,13 +19,14 @@ function stopGif() {
     clearInterval(intervalId);
 }
 
-// var timeoutId;
-// function startGif() {
-//     setTimeout(changeImage, 300);
-// }
+function startAlert() {
+    alert("The Gif is Starting!")
+}
 
+//pass it through a function, rather than declare it outside/have it floating
 
 function changeImage() {
+    //debugger;
     var imageSrc = document.getElementById("image").getAttribute("src");
     var currentImageNumber = imageSrc.substring(imageSrc.lastIndexOf("/") + 1, imageSrc.lastIndexOf("."));
     if (currentImageNumber >= 239) {
