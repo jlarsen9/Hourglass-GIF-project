2e79a3e227a5d3ba61e963b312b0ab4e1317da1e
Jenny Larsen <jennylarsen91@gmail.com>
2019-05-04 08:06:54 -0700

    message

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
-var button = document.createElement("button");
-document.body.appendChild(button);
-button.innerHTML = "CHANGE IMAGE";
+// var button = document.createElement("button");
+// document.body.appendChild(button);
+// button.innerHTML = "CHANGE IMAGE";
 
+
+function startGif() {
+    intervalId = setInterval(changeImage, 50);
+}
+
+function stopGif() {
+    clearInterval(intervalId);
+}
+
+// var timeoutId;
+// function startGif() {
+//     setTimeout(changeImage, 300);
+// }
 
 
 function changeImage() {
     var imageSrc = document.getElementById("image").getAttribute("src");
-    var currentImageNumber = imageSrc.substring(imageSrc.lastIndexOf("/") + 1, imageSrc.lastIndexOf("/") + 2);
-    var newImage = "/frames/" + (currentImageNumber++) + ".gif";
+    var currentImageNumber = imageSrc.substring(imageSrc.lastIndexOf("/") + 1, imageSrc.lastIndexOf("."));
+    if (currentImageNumber >= 239) {
+        currentImageNumber = 0;
+    }
+    var newImage = "/frames/" + (Number(currentImageNumber) + 1) + ".gif";
     document.getElementById("image").setAttribute("src", newImage);
     document.getElementById("result").innerHTML = newImage;
 }
 
-button.addEventListener('click', changeImage());
-//document.querySelector(button).addEventListener('click', changeImage());
+
 
 
 
